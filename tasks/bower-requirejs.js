b1ad92342ead3a96809e2cec5e5c313e58f7fae3
Sindre Sorhus <sindresorhus@gmail.com>
2014-05-06 04:23:43 +0200

    bump deps

--- tasks/bower-requirejs.js
+++ tasks/bower-requirejs.js
@@ -1,16 +1,13 @@
 'use strict';
-module.exports = function (grunt) {
+var bowerRequireJS = require('bower-requirejs');
 
+module.exports = function (grunt) {
   grunt.registerMultiTask('bower', 'Wire-up Bower components in RJS config', function () {
-    var cb = this.async();
-    var bowerRequireJS = require('bower-requirejs');
-    var options = this.options({
+    bowerRequireJS(this.options({
       config: this.data.rjsConfig,
       exclude: [],
       baseUrl: '',
       transitive: false
-    });
-
-    bowerRequireJS(options, cb);
+    }), this.async());
   });
 };
